009f4f31e138b2376e45cbdfd00db3586a49f799
John Napier <jcee.napier@gmail.com>
2021-11-30 14:50:39 -0700

    Add Passing tests and add welcomecontroller

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'welcome index page' do
   it 'displays a welcome page' do
-    visit "/welcome"
+    visit "/"
+    expect(page).to have_content("Welcome to Robin and John's Website!")
+    expect(page).to have_content("What would you like to do?")
+    expect(page).to have_link("Check out our shops!")
+  end
+
+  it 'can click shops index link' do 
+    visit "/"
+    expect(page).to have_link("Check out our shops!")
+    click_link "Check out our shops!"
+    expect(page).to have_current_path("/parents")
   end
 end
