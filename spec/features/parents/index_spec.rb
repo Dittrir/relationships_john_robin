10ec4cf489a3e9a1ddd521400c4d9652df1c93eb
John Napier <jcee.napier@gmail.com>
2021-11-30 16:29:37 -0700

    Add Tests to fix links on parent records page

--- spec/features/parents/index_spec.rb
+++ spec/features/parents/index_spec.rb
@@ -3,15 +3,21 @@ require 'rails_helper'
 RSpec.describe 'parents index page' do 
   it 'displays a parents page' do 
     visit "/parents"
-    expect(page).to have_content("Available Shops")
-    expect(page).to have_link("AutoShop")
-    expect(page).to have_link("PlumberShop")
+    expect(page).to have_content("Here are our parent records")
+    expect(page).to have_link("Auto Shops")
+    expect(page).to have_link("Add")
+    expect(page).to have_link("Vehicles")
+    expect(page).to have_link("Add")
+    expect(page).to have_link("Plumbing Shops")
+    expect(page).to have_link("Add")
+    expect(page).to have_link("Plumbers")
+    expect(page).to have_link("Add")
   end
 
-  it 'can click PlumberShop link' do 
-    visit "/parents" 
-    expect(page).to have_link("PlumberShop")
-    click_link "PlumberShop"
-    expect(page).to have_current_path("/parents/:id")
-  end
+  # it 'can click PlumberShop link' do 
+  #   visit "/parents" 
+  #   expect(page).to have_link("PlumberShop")
+  #   click_link "PlumberShop"
+  #   expect(page).to have_current_path("/parents/:id")
+  # end
 end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe 'welcome index page' do
     visit "/"
     expect(page).to have_content("Welcome to Robin and John's Website!")
     expect(page).to have_content("What would you like to do?")
-    expect(page).to have_link("Check out our shops!")
+    expect(page).to have_link("Check out our parent files!")
   end
 
   it 'can click shops index link' do 
     visit "/"
-    expect(page).to have_link("Check out our shops!")
-    click_link "Check out our shops!"
+    expect(page).to have_link("Check out our parent files!")
+    click_link "Check out our parent files!"
     expect(page).to have_current_path("/parents")
   end
 end
