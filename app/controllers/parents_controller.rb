7f405afe65a68a8773787646537d2e3ebec6f6bb
John Napier <jcee.napier@gmail.com>
2021-11-30 16:17:17 -0700

    Add Routes and parent controller

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,7 +1,12 @@
 class ParentsController < ApplicationController
   def index
   end
+
   def example
     @data = "random data goes here"
+  end
+
+  def id 
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#example'
-  
+
   get '/parents', to: 'parents#index'
+  get '/plumbershop', to: 'parents#id'
 end
