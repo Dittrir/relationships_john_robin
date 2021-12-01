a27f50c254e15f4915e72848bb743f44384680cd
Robin Dittrich <robinodittrich@gmail.com>
2021-11-30 23:55:55 -0800

    Repair autoshop database.

--- app/controllers/autoshops_controller.rb
+++ app/controllers/autoshops_controller.rb
@@ -1,16 +1,5 @@
 class AutoshopsController < ApplicationController
   def index
-    @autoshops = Autoshop.all
-  end
-
-  def new
-  end
-
-  def show
-    @autoshops = Autoshop.find(params[:id])
-  end
-
-  def edit
-    @autoshops = Autoshop.find(params[:id])
+    @auto_shops = Autoshop.all
   end
 end
