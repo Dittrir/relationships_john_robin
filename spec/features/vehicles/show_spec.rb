d37248d1b3b7168a102bf58db728d2db283632e3
Robin Dittrich <robinodittrich@gmail.com>
2021-12-01 16:47:44 -0800

    Add vehicle show view.

---
+++ spec/features/vehicles/show_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Vehicles Show Page", type: :feature do
+  it 'displays the vehicle name' do
+    vehicle_1 = Vehicle.create(shop_id: 1, name: 'Chevy Silverado 3500', need_repair: true, repair_cost: 3500)
+
+    visit "/vehicles/#{vehicle_1.id}"
+
+    expect(page).to have_content(vehicle_1.name)
+  end
+end
