fd0d4ce82d51e6e6ea8d8c3b3dc7781ddc836165
John Napier <jcee.napier@gmail.com>
2021-12-02 16:05:56 -0700

    User Story 1 complete

--- app/controllers/plumbershops_controller.rb
+++ app/controllers/plumbershops_controller.rb
@@ -1,20 +1,20 @@
 class PlumbershopsController < ApplicationController
   def index 
-    @plumbershops = PlumberShop.all
+    @plumbershops = Plumbershop.all
   end
 
-  def new; end
+#   def new; end
 
-  # def show
-  #   @plumbershop = PlumberShop.find(params[:id])
-  # end
+#   # def show
+#   #   @plumbershop = PlumberShop.find(params[:id])
+#   # end
 
-  def create 
-    shop = PlumberShop.new({
-      name: params[:plumbershop][:name],
-      service_offered: params[:plumbershop][:service_offered],
-      construction_offered: params[:plumbershop][:construction_offered],
-      fleet_vehicles: params[:plumbershopfleet_vehicles]
-    })
-  end
+#   def create 
+#     shop = PlumberShop.new({
+#       name: params[:plumbershop][:name],
+#       service_offered: params[:plumbershop][:service_offered],
+#       construction_offered: params[:plumbershop][:construction_offered],
+#       fleet_vehicles: params[:plumbershopfleet_vehicles]
+#     })
+#   end
 end
