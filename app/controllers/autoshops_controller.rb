dcfff0c0266d9fce31d063a74181f00b5b1f8ddc
Robin Dittrich <robinodittrich@gmail.com>
2021-12-05 15:13:56 -0800

    Add new, create, edit, and private methods to autoshops controller

--- app/controllers/autoshops_controller.rb
+++ app/controllers/autoshops_controller.rb
@@ -1,9 +1,32 @@
 class AutoshopsController < ApplicationController
   def index
-    @autoshops = Autoshop.all
+    @autoshops = Autoshop.sort_created_at
   end
 
   def show
     @autoshop = Autoshop.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    autoshop = Autoshop.create(autoshop_params)
+    redirect_to "/autoshops/#{autoshop.id}"
+  end
+
+  def edit
+    @autoshop = Autoshop.find(params[:id])
+  end
+
+  def update
+    @autoshop = Autoshop.find(params[:id])
+    @autoshop.update(autoshop_params)
+    redirect_to '/autoshops'
+  end
+
+private
+  def autoshop_params
+    params.permit(:name)
   end
 end
