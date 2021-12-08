6f597e204409d18b92e4d28f1c703ba339508f5f
John Napier <jcee.napier@gmail.com>
2021-12-08 16:20:32 -0700

    Refactor welcome page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,18 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'parents index page' do
-  it 'displays parents pages' do
-    visit "/"
-    expect(page).to have_content("Welcome to Robin and John's Website!")
-    expect(page).to have_content("What would you like to do?")
-    expect(page).to have_link("Check out our parent files!")
-  end
-
   it 'can click shops index link' do 
     visit "/"
-    expect(page).to have_link("Check out our parent files!")
-    click_link "Check out our parent files!"
+    
+    click_link "Let's get started!"
     expect(page).to have_current_path("/parents")
-    expect(page).to have_link("Let's get started!")
   end
 end
